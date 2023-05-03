dbdc28b6ede3e9bd3b6d286942cd25efc371674e
Hewr Srood <Hewr_s@outlook.com>
2023-05-03 17:02:51 +0300

    some stuff

--- src/componenets/Moement.tsx
+++ src/componenets/Moement.tsx
@@ -1,12 +1,11 @@
 import { ActivityIndicator, Dimensions, FlatList, StyleSheet, Text, View } from 'react-native';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import FeedFactory from './FeedFactory';
 import { FeedModel, IFeedResp } from '../Models/FeedModel';
 import withErroBoundary from '../HOC/withErroBoundary';
 import ErrorPage from './ErrorPage';
 
 const renderItem = ({ item }: { item: FeedModel }) => <FeedFactory {...item} />;
-
 const ListEmptyComponent = () => (
   <View
     style={{
@@ -19,31 +18,51 @@ const ListEmptyComponent = () => (
   </View>
 );
 const Moment = () => {
+  const keyExtractor = useCallback((item: FeedModel) => item.id.toString(), []);
   const [feed, setFeed] = useState<FeedModel[]>([]);
-  const fetchFeed = async () => {
-    const response = await fetch('https://my.api.mockaroo.com/manyface.json?key=0ecdabb0');
-    const mockData: IFeedResp[] = await response.json();
-    const data = mockData.map(item => new FeedModel(item));
-    setFeed(data);
-  };
+  const [loading, setLoading] = useState(false);
+
+  const fetchFeed = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch('https://my.api.mockaroo.com/manyface.json?key=0ecdabb0');
+      const mockData: IFeedResp[] = await response.json();
+      const data = mockData.map(item => new FeedModel(item));
+      setFeed(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
   useEffect(() => {
     console.log('FeedModal mounted');
     fetchFeed();
     return () => {
       console.log('FeedModal unmounted');
     };
-  }, []);
+  }, [fetchFeed]);
 
   return (
     <View style={{ flex: 1 }}>
-      <FlatList
-        style={{ flex: 1, paddingTop: 50 }}
-        contentContainerStyle={{ paddingBottom: 50, gap: 20 }}
-        keyExtractor={item => item.id.toString()}
-        data={feed}
-        renderItem={renderItem}
-        ListEmptyComponent={ListEmptyComponent}
-      />
+      {loading ? (
+        <ListEmptyComponent />
+      ) : (
+        <FlatList
+          style={{ flex: 1, paddingTop: 50 }}
+          contentContainerStyle={{ paddingBottom: 50, gap: 20 }}
+          keyExtractor={keyExtractor}
+          showsVerticalScrollIndicator={false}
+          data={feed}
+          renderItem={renderItem}
+          ListEmptyComponent={ListEmptyComponent}
+          initialNumToRender={10}
+          maxToRenderPerBatch={10}
+          windowSize={10}
+          removeClippedSubviews={true}
+        />
+      )}
     </View>
   );
 };
