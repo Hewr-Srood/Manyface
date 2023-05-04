cf65c5cfdc7a9831424260d3e1e2b44873cb9f50
Hewr Srood <Hewr_s@outlook.com>
2023-05-04 11:21:47 +0300

    add static data and implement nativeEventAnimation

--- src/componenets/LoadingInndicator.tsx
+++ src/componenets/LoadingInndicator.tsx
@@ -1,5 +1,5 @@
 import { Animated, Easing, StyleSheet, View, ViewStyle } from 'react-native';
-import React, { FC, useEffect, useRef } from 'react';
+import React, { Component, FC, useEffect, useRef } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 interface ILoadingInndicatorProps {
   style?: ViewStyle;
@@ -43,43 +43,80 @@ export const RotatingCircle: FC<ILoadingInndicatorProps> = ({
     </View>
   );
 };
-export const SpinningEnvelope: FC<ILoadingInndicatorProps> = ({
-  style,
-  size = 20,
-  color = 'gray',
-}) => {
-  const rotate = useRef(new Animated.Value(0)).current;
-  useEffect(() => {
+// export const SpinningEnvelope: FC<ILoadingInndicatorProps> = ({
+//   style,
+//   size = 20,
+//   color = 'gray',
+// }) => {
+//   const rotate = useRef(new Animated.Value(0)).current;
+//   useEffect(() => {
+//     Animated.loop(
+//       Animated.timing(rotate, {
+//         toValue: 1,
+//         duration: 1000,
+//         easing: Easing.linear,
+//         useNativeDriver: true,
+//       }),
+//     ).start();
+//   }, []);
+//   return (
+//     <View style={[style, styles.stablizer]}>
+//       <AnimatedIcon
+//         name='mail'
+//         size={size}
+//         color={color}
+//         style={{
+//           transform: [
+//             {
+//               rotateY: rotate.interpolate({
+//                 inputRange: [0, 1],
+//                 outputRange: ['0deg', '360deg'],
+//               }),
+//             },
+//           ],
+//         }}
+//       />
+//     </View>
+//   );
+// };
+export class SpinningEnvelope extends Component<ILoadingInndicatorProps> {
+  constructor(props: ILoadingInndicatorProps) {
+    super(props);
+  }
+  rotate = new Animated.Value(0);
+
+  componentDidMount(): void {
     Animated.loop(
-      Animated.timing(rotate, {
+      Animated.timing(this.rotate, {
         toValue: 1,
         duration: 1000,
         easing: Easing.linear,
         useNativeDriver: true,
       }),
     ).start();
-  }, []);
-  return (
-    <View style={[style, styles.stablizer]}>
-      <AnimatedIcon
-        name='mail'
-        size={size}
-        color={color}
-        style={{
-          transform: [
-            {
-              rotateY: rotate.interpolate({
-                inputRange: [0, 1],
-                outputRange: ['0deg', '360deg'],
-              }),
-            },
-          ],
-        }}
-      />
-    </View>
-  );
-};
-
+  }
+  render() {
+    return (
+      <View style={[this.props.style, styles.stablizer]}>
+        <AnimatedIcon
+          name='loader'
+          size={this.props.size}
+          color={this.props.color}
+          style={{
+            transform: [
+              {
+                rotate: this.rotate.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: ['0deg', '360deg'],
+                }),
+              },
+            ],
+          }}
+        />
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   stablizer: {
     justifyContent: 'center',
--- src/componenets/Moement.tsx
+++ src/componenets/Moement.tsx
@@ -1,11 +1,20 @@
-import { ActivityIndicator, Dimensions, FlatList, StyleSheet, Text, View } from 'react-native';
-import { useEffect, useState, useCallback } from 'react';
+import {
+  ActivityIndicator,
+  Animated,
+  Dimensions,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { useEffect, useState, useCallback, useRef } from 'react';
 import FeedFactory from './FeedFactory';
 import { FeedModel, IFeedResp } from '../Models/FeedModel';
 import withErroBoundary from '../HOC/withErroBoundary';
 import ErrorPage from './ErrorPage';
-import { RotatingCircle } from './LoadingInndicator';
-
+import { RotatingCircle, SpinningEnvelope } from './LoadingInndicator';
+import data from '../data.json';
+const LoomingSpinnigEnvelope = Animated.createAnimatedComponent(SpinningEnvelope);
 const renderItem = ({ item }: { item: FeedModel }) => <FeedFactory {...item} />;
 const ListEmptyComponent = () => (
   <View
@@ -18,18 +27,51 @@ const ListEmptyComponent = () => (
     <RotatingCircle size={40} />
   </View>
 );
+const loadingIndicatorOffset = 50;
 const Moment = () => {
   const keyExtractor = useCallback((item: FeedModel) => item.id.toString(), []);
+  const pullDownPos = useRef(new Animated.Value(0)).current;
+  const autoScrolling = useRef(new Animated.Value(0)).current;
+  const userPulling = useRef(new Animated.Value(0)).current;
+  const flatListRef = useRef<FlatList<FeedModel>>(null);
+
   const [feed, setFeed] = useState<FeedModel[]>([]);
   const [loading, setLoading] = useState(false);
-
+  const beginDrag = () => {
+    userPulling.setValue(1);
+    autoScrolling.setValue(0);
+  };
+  const endDrag = e => {
+    const y = e.nativeEvent.contentOffset.y;
+    userPulling.setValue(0);
+    if (y <= -loadingIndicatorOffset) {
+      setLoading(true);
+      setTimeout(() => {
+        flatListRef?.current?.scrollToOffset({ offset: 0, animated: true });
+        setLoading(false);
+      }, 1000);
+    }
+  };
+  const onReset = e => {
+    const y = e.nativeEvent.contentOffset.y;
+    if (y === 0) {
+      userPulling.setValue(0);
+      autoScrolling.setValue(0);
+    }
+    if (loading) {
+      setLoading(false);
+    }
+  };
+  const onScroll = Animated.event([{ nativeEvent: { contentOffset: { y: pullDownPos } } }], {
+    useNativeDriver: true,
+  });
   const fetchFeed = useCallback(async () => {
     setLoading(true);
     try {
-      const response = await fetch('https://my.api.mockaroo.com/manyface.json?key=0ecdabb0');
-      const mockData: IFeedResp[] = await response.json();
-      const data = mockData.map(item => new FeedModel(item));
-      setFeed(data);
+      // const response = await fetch('https://my.api.mockaroo.com/manyface.json?key=0ecdabb0');
+      // const mockData: IFeedResp[] = await response.json();
+      const res = data.map(item => new FeedModel(item));
+      setFeed(res);
     } catch (error) {
       console.error(error);
     } finally {
@@ -47,25 +89,60 @@ const Moment = () => {
 
   return (
     <View style={{ flex: 1 }}>
-      {loading ? (
-        <ListEmptyComponent />
-      ) : (
-        <FlatList
-          style={{ flex: 1, paddingTop: 50 }}
-          contentContainerStyle={{ paddingBottom: 50, gap: 20 }}
-          keyExtractor={keyExtractor}
-          showsVerticalScrollIndicator={false}
-          data={feed}
-          renderItem={renderItem}
-          ListEmptyComponent={ListEmptyComponent}
-          initialNumToRender={10}
-          maxToRenderPerBatch={10}
-          windowSize={10}
-          removeClippedSubviews={true}
+      <Animated.FlatList
+        contentInset={{ top: loading ? 5 : 0 }}
+        ref={flatListRef}
+        style={{ flex: 1, paddingTop: 50 }}
+        contentContainerStyle={{ paddingBottom: 50, gap: 20 }}
+        keyExtractor={keyExtractor}
+        showsVerticalScrollIndicator={false}
+        data={feed}
+        renderItem={renderItem}
+        ListEmptyComponent={ListEmptyComponent}
+        onScroll={onScroll}
+        onScrollBeginDrag={beginDrag}
+        onScrollEndDrag={endDrag}
+        onMomentumScrollEnd={onReset}
+      />
+      <View style={styles.overlay}>
+        <LoomingSpinnigEnvelope
+          color={'#6291f0'}
+          size={50}
+          style={{
+            opacity: Animated.add(
+              Animated.multiply(
+                userPulling,
+                pullDownPos.interpolate({
+                  inputRange: [-loadingIndicatorOffset, 1],
+                  outputRange: [0.5, 0],
+                }),
+              ),
+              Animated.multiply(
+                autoScrolling,
+                pullDownPos.interpolate({
+                  inputRange: [-loadingIndicatorOffset, 0],
+                  outputRange: [1, 0],
+                }),
+              ),
+            ),
+          }}
         />
-      )}
+      </View>
+      {/* )} */}
     </View>
   );
 };
+const styles = StyleSheet.create({
+  overlay: {
+    position: 'absolute',
+    justifyContent: 'center',
+    width: '100%',
+    top: 10,
+    height: loadingIndicatorOffset,
+    // backgroundColor: 'lightgray',
+    opacity: 0.5,
+    zIndex: 1,
+  },
+});
 
 export default withErroBoundary(Moment, ErrorPage, undefined);
